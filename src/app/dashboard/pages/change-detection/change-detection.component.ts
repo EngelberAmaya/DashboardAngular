e2508a7b30645d1c70913410da2a0e1d08c20f56
EngelberAmaya <engelberamaya@gmail.com>
2024-07-22 14:23:39 -0400

    ChangeDetection | OnPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, CommonModule ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './change-detection.component.html',
   styles: ``
 })
 export default class ChangeDetectionComponent {
 
+   public currentFramework = computed(
+     () => `Change Detection - ${this.frameworkAsSignal().name}`
+   )
+
+    public frameworkAsSignal = signal({
+        name: 'Angular',
+        release: 2024
+    });
+
+    public frameworkAsProperty = {
+      name: 'Angular',
+      release: 2024
+    };
+
+    constructor() {
+      setTimeout(() => {
+        //this.frameworkAsProperty.name = 'React';
+        this.frameworkAsSignal.update(value => ({
+          ...value,
+          name: 'React'
+        }))
+
+        console.log('Hecho');
+      }, 3000);
+    }
 }
