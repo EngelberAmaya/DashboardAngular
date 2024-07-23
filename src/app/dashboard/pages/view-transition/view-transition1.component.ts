2176603ecb80f48f37ab2d29c319f513789b3825
EngelberAmaya <engelberamaya@gmail.com>
2024-07-23 10:18:53 -0400

    Hero Animation | View Transitions

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -9,10 +9,10 @@ import { TitleComponent } from '@shared/title/title.component';
 
     <section class="flex justify-start">
         <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum"
-            width="200" height="300"
+            width="200" height="300" style="view-transition-name: hero1;"
         />
 
-        <div class="bg-blue-500 w-56 h-56">
+        <div class="bg-blue-500 w-56 h-56" style="view-transition-name: hero2;">
           
         </div>
     </section>
