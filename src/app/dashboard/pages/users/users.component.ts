9dd2903730f8fe63aabf99290e188e5c03a90b14
EngelberAmaya <engelberamaya@gmail.com>
2024-07-23 11:38:20 -0400

    Peticiones HTTP | importProvidersFrom

--- src/app/dashboard/pages/users/users.component.ts
+++ src/app/dashboard/pages/users/users.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { UsersService } from '../../../services/users.service';
+import { TitleComponent } from '@shared/title/title.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [TitleComponent, RouterModule],
   templateUrl: './users.component.html',
   styles: ``
 })
